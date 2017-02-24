e1ea10eea5109d80aa61bc67f8bc5e48b5eb9620
Josh Starcher <josh.starcher@gmail.com>
2017-02-24 10:43:52 -0500

    Add more fields

--- app/admin/entity.rb
+++ app/admin/entity.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Entity do
 
-  permit_params :name, :legal_name, :legal_other, :address, :telephone, :fax, :email, :dp_reg, :dp_contact, :signatory, :is_eea, :country
+  permit_params :name, :legal_name, :legal_other, :address, :telephone, :fax, :email, :dp_reg, :dp_contact, :signatory, :is_eea, :country, :area, :is_areaoffice, :description
 
 end
