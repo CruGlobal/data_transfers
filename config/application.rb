ad9527b2ec165faea0d7051fa3038fa4e14df289
Justin Sabelko <justin.sabelko@cru.org>
2019-03-12 14:16:07 -0500

    Upgrade to Rails 5.1.6.1

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module DataTransfers
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     config.assets.enabled = false
     config.generators do |g|
       g.test_framework :rspec, fixture: true
