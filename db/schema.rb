fc2349ead783a3adb5efbddce6948ac188111360
Justin Sabelko <justin.sabelko@cru.org>
2021-08-06 14:37:08 -0500

    Regenerate schema.db from production db

--- db/schema.rb
+++ db/schema.rb
@@ -1,96 +1,100 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316132743) do
+ActiveRecord::Schema.define(version: 2016_03_16_132743) do
 
-  create_table "entities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.string  "name",                      default: "",    null: false
-    t.string  "legal_name",                default: "",    null: false
-    t.text    "legal_other", limit: 65535
-    t.text    "address",     limit: 65535
-    t.text    "telephone",   limit: 65535
-    t.text    "fax",         limit: 65535
-    t.text    "email",       limit: 65535
-    t.text    "dp_reg",      limit: 65535
-    t.text    "dp_contact",  limit: 65535
-    t.text    "signatory",   limit: 65535
-    t.boolean "is_eea",                    default: false, null: false
-    t.text    "country",     limit: 65535,                 null: false
+  create_table "countries", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", comment: "From https://www.worlddata.info/downloads/", force: :cascade do |t|
+    t.text "country"
+    t.text "code"
+    t.text "lat"
+    t.text "lon"
   end
 
-  add_index "entities", ["name"], name: "entityname", unique: true, using: :btree
+  create_table "entities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "legal_name", default: "", null: false
+    t.text "legal_other"
+    t.text "address"
+    t.text "telephone"
+    t.text "fax"
+    t.text "email"
+    t.text "dp_reg"
+    t.text "dp_contact"
+    t.text "signatory"
+    t.boolean "is_eea", default: false, null: false
+    t.text "country", null: false
+    t.text "area"
+    t.boolean "is_areaoffice", default: false, null: false
+    t.text "description"
+    t.index ["name"], name: "entityname", unique: true
+  end
 
-  create_table "entity_transfers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.integer "entity_id",    limit: 5,       null: false
-    t.integer "transfer_id",  limit: 5,       null: false
+  create_table "entity_transfers", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.integer "entity_id", null: false
+    t.integer "transfer_id", null: false
     t.boolean "is_recipient", default: false, null: false
+    t.index ["entity_id"], name: "fk_entities_transfers_entities_1"
+    t.index ["transfer_id"], name: "fk_entities_transfers_transfers_1"
   end
 
-  add_index "entity_transfers", ["entity_id"], name: "fk_entities_transfers_entities_1", using: :btree
-  add_index "entity_transfers", ["transfer_id"], name: "fk_entities_transfers_transfers_1", using: :btree
-
-  create_table "purpose_transfers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.integer "purpose_id",  limit: 5,  null: false
-    t.integer "transfer_id", limit: 5, null: false
+  create_table "purpose_transfers", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.integer "purpose_id", null: false
+    t.integer "transfer_id", null: false
+    t.index ["purpose_id"], name: "fk_purposes_transfers_purposes_1"
+    t.index ["transfer_id"], name: "fk_purposes_transfers_transfers_1"
   end
 
-  add_index "purpose_transfers", ["purpose_id"], name: "fk_purposes_transfers_purposes_1", using: :btree
-  add_index "purpose_transfers", ["transfer_id"], name: "fk_purposes_transfers_transfers_1", using: :btree
-
-  create_table "purposes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.string "name",                           default: "", null: false
-    t.text   "full_description", limit: 65535,              null: false
-    t.text   "data_subjects",    limit: 65535,              null: false
-    t.text   "data_categories",  limit: 65535,              null: false
-    t.text   "data_recipients",  limit: 65535,              null: false
-    t.text   "additional_info",  limit: 65535
-    t.text   "storage_limit",    limit: 65535,              null: false
-    t.text   "data_sensitive",   limit: 65535,              null: false
-    t.text   "security_policy",  limit: 65535,              null: false
+  create_table "purposes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.text "full_description", null: false
+    t.text "data_subjects", null: false
+    t.text "data_categories", null: false
+    t.text "data_recipients", null: false
+    t.text "additional_info"
+    t.text "storage_limit", null: false
+    t.text "data_sensitive", null: false
+    t.text "security_policy", null: false
+    t.text "context"
+    t.index ["name"], name: "purposename", unique: true
   end
 
-  add_index "purposes", ["name"], name: "purposename", unique: true, using: :btree
-
-  create_table "transfer_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.string "name",          default: "", null: false
+  create_table "transfer_types", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "template_name"
+    t.text "context", size: :tiny
+    t.index ["name"], name: "transfertypename", unique: true
   end
 
-  add_index "transfer_types", ["name"], name: "transfertypename", unique: true, using: :btree
-
-  create_table "transfers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT" do |t|
-    t.integer "transfer_type_id", limit: 5
-    t.string  "name"
+  create_table "transfers", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.integer "transfer_type_id"
+    t.string "name"
+    t.index ["name"], name: "transfername", unique: true
+    t.index ["transfer_type_id"], name: "fk_transfers_transfertype_1"
   end
 
-  add_index "transfers", ["name"], name: "transfername", unique: true, using: :btree
-  add_index "transfers", ["transfer_type_id"], name: "fk_transfers_transfertype_1", using: :btree
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "username",           default: "", null: false
-    t.string   "sso_guid",                        null: false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "sign_in_count",      default: 0,  null: false
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "sso_guid", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
-
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "entity_transfers", "entities", name: "fk_entities_transfers_entities_1"
   add_foreign_key "entity_transfers", "transfers", name: "fk_entities_transfers_transfers_1"
