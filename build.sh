dcf748007b095af600f912dbc209c8c407214275
Brian Zoetewey <brian.zoetewey@cru.org>
2022-03-03 16:41:48 -0500

    Lookup redis/pg container IPs

--- build.sh
+++ build.sh
@@ -2,18 +2,20 @@
 
 docker run --rm --network=$DOCKER_NETWORK --name=$PROJECT_NAME-redis -d redis
 docker run --rm --network=$DOCKER_NETWORK --name=$PROJECT_NAME-pg -e POSTGRES_PASSWORD=data_transfers -d postgres:13
-sleep 2
+sleep 10
+
+REDIS_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $PROJECT_NAME-redis)
+PG_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $PROJECT_NAME-pg)
 
 docker buildx build $DOCKER_ARGS \
     --build-arg DB_ENV_POSTGRESQL_USER=postgres \
     --build-arg DB_ENV_POSTGRESQL_PASS=data_transfers \
-    --build-arg DB_PORT_5432_TCP_ADDR=$PROJECT_NAME-pg \
-    --build-arg REDIS_PORT_6379_TCP_ADDR=$PROJECT_NAME-redis \
-    --build-arg SECRET_KEY_BASE=SECRET_KEY_BASE \
+    --build-arg DB_PORT_5432_TCP_ADDR=$PG_IP \
+    --build-arg REDIS_PORT_6379_TCP_ADDR=$REDIS_IP \
+    --build-arg SECRET_KEY_BASE=$SECRET_KEY_BASE \
     --build-arg DD_API_KEY=$DD_API_KEY \
     .
 rc=$?
-
 
 docker stop $PROJECT_NAME-redis $PROJECT_NAME-pg
 
